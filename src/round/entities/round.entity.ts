039e0f78c68d18201e2a55922ec366ed5da2666f
thejiw <kamornpisit03@gmail.com>
2022-08-29 13:51:57 +0700

    Fix: relation on entity

--- src/round/entities/round.entity.ts
+++ src/round/entities/round.entity.ts
@@ -1,1 +1,36 @@
-export class Round {}
+import { ApiProperty } from '@nestjs/swagger'
+import { Base } from 'src/common/base/entities/base.entity'
+import { RoundType } from 'src/round-type/entities/round-type.entity'
+import { Column, Entity, Index, JoinColumn, OneToMany, UpdateDateColumn } from 'typeorm'
+
+@Entity('round')
+@Index('IDX_ROUND_ID')
+export class Round extends Base {
+  @ApiProperty()
+  @Column({ unique: true, nullable: false })
+  name: string
+
+  @ApiProperty()
+  @UpdateDateColumn({ name: 'start_dete', nullable: true })
+  startDate: Date
+
+  @ApiProperty()
+  @UpdateDateColumn({ name: 'end_date', nullable: true })
+  endDate: Date
+
+  @ApiProperty()
+  @Column({ unique: false, nullable: true })
+  day: string
+
+  @ApiProperty()
+  @Column({ unique: false, nullable: true })
+  date: string
+
+  // ---------- start relation ----------//
+
+  @OneToMany(() => RoundType, (roundType) => roundType.round)
+  @JoinColumn({ name: 'round_type_id' })
+  roundType: RoundType[]
+
+  // ---------- end relation ----------//
+}
