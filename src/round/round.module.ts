c30f85822fab21845f91d351e2c3f9cfffc7b043
thejiw <kamornpisit03@gmail.com>
2022-09-01 11:57:31 +0700

    Update: round management api

--- src/round/round.module.ts
+++ src/round/round.module.ts
@@ -4,10 +4,11 @@ import { RoundController } from './round.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from 'src/auth/auth.module'
 import { RoundRepository } from './round.repository'
+import { RoundManagementController } from './round-management.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([RoundRepository]), forwardRef(() => AuthModule)],
-  controllers: [RoundController],
+  controllers: [RoundManagementController, RoundController],
   providers: [RoundService]
 })
 export class RoundModule {}
